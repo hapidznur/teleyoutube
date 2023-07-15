6e43eeee452e5bc9988564a78f9eca748b3009c4
hafid hidayatullah <hafid@majoo.id>
2023-07-15 22:13:38 +0700

    use dispatcher

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,35 @@
-use teloxide::{prelude::*, utils::command::BotCommands};
-use std::process::Command;
+use teloxide::{
+    prelude::*, 
+    payloads::SendMessaageSetters,
+};
+use std::error::Error;
+
+fn message_handler(
+    m: Message, 
+    bot: Bot
+) -> UpdateHandler<Box<dyn Error + Send + Sync + 'static>> {
+    let chat = &m.chat;
+    let username = chat.username().map(String::from);
+
+    if let Some(maybe_url) = m.text() {
+        if maybe_url == "/start" {
+            bot.send_message(chat.id, format!("Your username is send url instead")).await?;
+        }
+    }
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
+    teloxide::enable_logging!();
     log::info!("Starting command bot...");
 
-    let bot = Bot::from_env();
-
-    Command::repl(bot, answer).await;
+    let bot = Bot::from_env().auto_send();
+    
+    Dispatcher::builder(bot, message_handler)
+    .enable_ctrlc_handler()
+    .build()
+    .dispatch()
+    .await;
 }
-
-#[derive(BotCommands, Clone)]
-#[command[rename_rule = "lowercase", description = "These commands are supported:"]]
-enum CommandBot {
-    #[command(description = "display this text")]
-    Help, 
-    #[command(description = "handle username")]
-    Username(String),
-    #[command(description = "handle a username and a age.", parse_with = "split")]
-    UsernameAndAge {username: String, age: u8},
-}
-
-async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    match cmd {
-        CommandBot::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
-        CommandBot::Username(username) => {
-            let lscommand = Command::new("ls").Spawn()
-            log::info!("Starting command bot...");
-            bot.send_message(msg.chat.id, format!("Your username is @{lscommand}")).await?
-        }
-        CommandBot::UsernameAndAge { username, age } => {
-            bot.send_message(msg.chat.id, format!("@{username} and @{age}.")).await?
-        } 
-    };
-    Ok(())
-}
-
